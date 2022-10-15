eb68c28e4961ff02fe8f322e532f091b209a280d
iamageo <geovani.af4@gmail.com>
2022-10-15 20:51:03 -0300

    creating dao and database structure

---
+++ app/src/main/java/com/iamageo/plantae/data/datasource/PlantDatabase.kt
@@ -0,0 +1,19 @@
+package com.iamageo.plantae.data.datasource
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.iamageo.plantae.data.model.Plant
+
+@Database(
+    entities = [Plant::class],
+    version = 1
+)
+abstract class PlantDatabase : RoomDatabase() {
+
+    abstract val plantaeDAO: PlantaeDAO
+
+    companion object {
+        const val DATABASE_NAME = "plantaedb"
+    }
+
+}
---
+++ app/src/main/java/com/iamageo/plantae/data/datasource/PlantaeDAO.kt
@@ -0,0 +1,22 @@
+package com.iamageo.plantae.data.datasource
+
+import androidx.room.*
+import com.iamageo.plantae.data.model.Plant
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PlantaeDAO {
+
+    @Query("SELECT * FROM plant")
+    fun getAllPlants(): Flow<List<Plant>>
+
+    @Query("SELECT * FROM plant WHERE id = :id")
+    suspend fun getPlantsById(id: Int): Plant?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPlant(plant: Plant)
+
+    @Delete
+    suspend fun deletePlant(plant: Plant)
+
+}
