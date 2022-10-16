4663cad681248a66721b3234d533d9adfd929277
iamageo <geovani.af4@gmail.com>
2022-10-16 14:26:42 -0300

    implementing basic of navigation

--- app/src/main/java/com/iamageo/plantae/PlantMainActivity.kt
+++ app/src/main/java/com/iamageo/plantae/PlantMainActivity.kt
@@ -6,29 +6,37 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.iamageo.plantae.ui.screens.home.PlantHomeScreen
 import com.iamageo.plantae.ui.theme.PlantaeTheme
 
 class PlantMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             PlantaeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = PlantaeScreens.PlantHomeScreen.route
+                    ) {
+                        composable(route = PlantaeScreens.PlantHomeScreen.route) {
+                            PlantHomeScreen(navController = navController)
+                        }
+                    }
                 }
             }
+
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
